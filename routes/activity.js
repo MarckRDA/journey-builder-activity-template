e028d11fd9c8f095564a1f6e67b5eb731f381dea
Marcos <Marcos.RochaDaviAlves@br.experian.com>
2023-11-21 13:16:33 -0300

    sS

--- routes/activity.js
+++ routes/activity.js
@@ -29,7 +29,7 @@ function logData(req) {
         secure: req.secure,
         originalUrl: req.originalUrl
     });
-    console.log("body: " + JSON.stringify(req.body.data.toJSON()));
+    console.log("body: " + JSON.stringify(req.body?.data?.toJSON()));
     console.log("headers: " + req.headers);
     console.log("trailers: " + req.trailers);
     console.log("method: " + req.method);
